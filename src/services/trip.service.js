43b5230aec3296201fcd8c427e0dfa512e1ad13a
RidmiAhinshika <ridmi2.desilva@gmail.com>
2025-09-23 19:56:27 +0530

    remove trip service functions and module exports

--- src/services/trip.service.js
+++
@@ -1,9 +0,0 @@
-const Trip = require('../models/Trip');
-
-async function create(data) { return Trip.create(data); }
-async function list(q = {}) { return Trip.find(q); }
-async function get(id) { return Trip.findById(id); }
-async function update(id, data) { return Trip.findByIdAndUpdate(id, data, { new: true }); }
-async function remove(id) { return Trip.findByIdAndDelete(id); }
-
-module.exports = { create, list, get, update, remove };
