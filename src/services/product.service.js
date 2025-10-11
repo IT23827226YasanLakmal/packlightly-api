59da0c63b5de4628797ffbe4a65db983a92cac42
thisugithub <thisuripathirathna2001@gmail.com>
2025-10-11 14:29:21 +0530

    refactor: Refactor product service methods for cleaner codebase

--- src/services/product.service.js
+++
@@ -1,9 +0,0 @@
-const Product = require('../models/Product');
-
-async function create(data) { return Product.create(data); }
-async function list() { return Product.find(); }
-async function get(id) { return Product.findById(id); }
-async function update(id, data) { return Product.findByIdAndUpdate(id, data, { new: true }); }
-async function remove(id) { return Product.findByIdAndDelete(id); }
-
-module.exports = { create, list, get, update, remove };
